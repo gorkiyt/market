a60076460b77fbd4280f175d74e6197e6a031e06
gorkiyt <hgrkm5@gmail.com>
2016-05-25 13:15:06 +0300

    Update MarketSearch.php

--- models/MarketSearch.php
+++ models/MarketSearch.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace frontend\modules\admin\models;
+namespace gorkiyt\market\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use frontend\modules\admin\models\Market;
+use gorkiyt\market\models\Market;
 
 /**
  * MarketSearch represents the model behind the search form about `frontend\models\Market`.
